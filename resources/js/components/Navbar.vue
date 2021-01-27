07527169a483fa984c58c93a11ba1d98853b42be
rea-jin <56153015+rea-jin@users.noreply.github.com>
2021-01-27 17:17:07 +0900

    変更されたファイルがたくさんされたファイルがたくさんあるが、よくわからない

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -4,27 +4,43 @@
         VueApi
         </router-link>
         <div class="navbar__menu">
+
+            <!-- ログインしていたら、投稿ボタンを表示 -->
             <div v-if="isLogin" class="navbar__item">
-                <button class="button">
-                    <i class="icon ion-md-add"></i>
-                    Subimit a Item
-                </button>
+              <!-- クリックでフォームを表示 -->
+              <button class="button" @click="showForm = ! showForm">
+                <i class="icon ion-md-add"></i>
+                商品を投稿する
+              </button>
             </div>
-            <span v-if="isLogin" class="navbar__item">
+
             <!-- ログインしているときusername表示 -->
+            <span v-if="isLogin" class="navbar__item">
                 {{ username }}
             </span>
+            
             <div v-else class="navbar__item">
-                <router-link to="login" class="button button--link">
-                Login / Register
+                <router-link class="button button--link" to="/login">
+                ログイン / 登録
                 </router-link>
             </div>
         </div>
+        <ItemForm v-model="showForm" />
     </nav>
 </template>
 
 <script>
+import ItemForm from './ItemForm.vue'
+
 export default {
+  components:{
+    ItemForm
+  },
+    data () {
+    return {
+      showForm: false
+    }
+  },
   computed: {
     isLogin () {
       return this.$store.getters['auth/check']
